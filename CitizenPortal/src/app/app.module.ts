349013ad42cff29b492cc6df3df819deecae78ec
fayza33amreen <fayza33amreen@gmail.com>
2018-11-10 11:20:56 +0600

    Routing between Login and Registration

--- CitizenPortal/src/app/app.module.ts
+++ CitizenPortal/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material'
 
 import { AppComponent } from './app.component';
-import { ApproutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignUpDialogComponent } from './sign-up-dialog/sign-up-dialog.component';
@@ -26,14 +25,15 @@ import { LoginComponent } from './login/login.component';
   ],
 
   imports: [
-    ApproutingModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    MaterialModule,
+    MaterialModule
   ],
 
   entryComponents: [
+    DashboardComponent,
     SignUpDialogComponent
   ],
 
