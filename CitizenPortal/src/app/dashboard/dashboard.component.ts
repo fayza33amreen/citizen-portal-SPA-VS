25bd68a532c97a5fbe0fdf7ed36507de31e06302
fayza33amreen <fayza33amreen@gmail.com>
2018-11-19 18:55:49 +0600

    Responsive grid row

--- CitizenPortal/src/app/dashboard/dashboard.component.ts
+++ CitizenPortal/src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  row1Span: number = 1;
+  flag1: number = 0;
+  panelState1: boolean;
+  row2Span: number = 1;
+  flag2: number = 0;
+  panelState2: boolean;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  panelStateOpen1() {
+    this.panelState1 = true;
+    this.row1Span = 2;
+    this.flag1++;
+  }
+  panelStateClose1() {
+    this.panelState1 = false;
+    this.flag1--;
+    if (!this.flag1)
+      this.row1Span = 1;
+  }
+
+  panelStateOpen2() {
+    this.panelState2 = true;
+    this.row2Span = 2;
+    this.flag2++;
+  }
+  panelStateClose2() {
+    this.panelState2 = false;
+    this.flag2--;
+    if (!this.flag2)
+      this.row2Span = 1;
+  }
 }
